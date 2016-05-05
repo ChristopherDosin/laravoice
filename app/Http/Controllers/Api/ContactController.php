d31bb98c89151a0b501bb8c368086b19099e4340
hostianer <info@hostianer.de>
2016-05-05 17:11:51 +0200

    Updated Contact Controller

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Contact;
-use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -11,11 +10,6 @@ use App\Http\Controllers\Controller;
 
 class ContactController extends Controller
 {
-    public function getContacts(User $user)
-    {
-        return $user->with('usertype', 'billingaddress')->get();
-    }
-
     public function saveContact(Request $request, Contact $contact)
     {
         $contact->create($request->all());
